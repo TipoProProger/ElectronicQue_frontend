bb51bf690849860e68f60565c4d5a5fab4796ae8
TipoProProger <mishasmirnow2011@mail.ru>
2021-03-16 10:26:27 +0300

    add page and component
    Add: main page, header component, component decomposition

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
+import { MatGridListModule } from '@angular/material/grid-list'; 
+import { MatInputModule } from '@angular/material/input';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainWindowComponent } from './pages/main-window/main-window.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,    
+    MainWindowComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    MatGridListModule,
+    MatInputModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { DatePipe } from '@angular/common';
+
+@Component({
+  selector: 'header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+
+  @Input() FIO: string = "Введите ФИО специалиста";
+  currentDate = new Date();
+  currentTime = this.currentDate.getMinutes();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
